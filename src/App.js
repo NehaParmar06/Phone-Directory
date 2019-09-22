66b89ae69a0f5fd71369f3b866300ac47b1885ef
NehaParmar06 <neha.parmar06@gmail.com>
2019-09-22 19:02:33 +0530

    Basic Skeleton of Phone Directory App

--- src/App.js
+++ src/App.js
@@ -4,7 +4,18 @@ class App extends Component{
   render(){
     return(
       <div>
-
+        <div className="header">
+        <div>
+        Phone Directory
+        </div>
+      </div>
+      <button>Add</button>
+      <div>
+        <span>Name</span><br/>
+        <span>Phone</span>
+      </div>
+      <label htmlFor="name">Name:</label>
+      <input type="text" placeholder="Your Name"></input>
       </div>
     );
   }
